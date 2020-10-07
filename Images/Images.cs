727aab01fb993476ddac42a12c081f7b9f092f14
PintTheDragon <PintTheDragon@hotmail.com>
2020-10-07 12:42:13 -0700

    Bump version

--- Images/Images.cs
+++ Images/Images.cs
@@ -15,7 +15,7 @@ namespace Images
     {
         public override string Name { get; } = "Images";
         public override string Author { get; } = "PintTheDragon";
-        public override Version Version { get; } = new Version(1, 1, 0);
+        public override Version Version { get; } = new Version(1, 1, 1);
 
         internal static Images Singleton;
         internal string IntercomText = null;
